2389fb3804f29dcf11acc744255015668522aa8b
AndeeeeeZ <zhuandeee@gmail.com>
2026-08-08 20:26:50 -0400

    Basic eating food mechanic

--- Assets/Multiplayer/Scripts/Food/MP_Food.cs
+++ Assets/Multiplayer/Scripts/Food/MP_Food.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
-using PurrNet; 
+using PurrNet;
 
 public class MP_Food : NetworkBehaviour
 {
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isServer)
+            return; 
+
+        MP_PlayerMovement player = collision.GetComponent<MP_PlayerMovement>(); 
+
+        if (player = null)
+            return; 
+        
+        Debug.Log("Eat one food"); 
+        Destroy(gameObject); 
+    }
 }
